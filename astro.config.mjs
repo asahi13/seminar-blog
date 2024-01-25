b51fe1672bc245c49b55db2af213369a66bd3268
asahi13 <5150tah@gmail.com>
2024-01-25 17:34:49 +0900

    初回コミット

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
 import { defineConfig } from 'astro/config';
+import { fileURLToPath } from "url";
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
+import path, { dirname } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+	vite: {
+		resolve: {
+		  alias: {
+			"@/": `${path.resolve(__dirname, "src")}/`,
+		  },
+		},
+	  },
 });
